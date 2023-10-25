a09f4475d7e8cffb59cc5cac82511d8e0ad0e97a
junny97 <pcohad12@gmail.com>
2023-10-25 15:55:13 +0900

    Chore: recoil 설치#1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from './styles/theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+);
