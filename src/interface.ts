84c4e5d8d8f05d98fa115d65b70cf863111e4157
junny97 <pcohad12@gmail.com>
2023-10-31 16:04:52 +0900

    Refactor:  interface.ts 파일 만든후 type 정의하기#6

---
+++ src/interface.ts
@@ -0,0 +1,15 @@
+export const enum Categories {
+  'TO_DO' = 'TO_DO',
+  'DOING' = 'DOING',
+  'DONE' = 'DONE',
+}
+
+export interface IToDo {
+  text: string;
+  id: number;
+  category: Categories;
+}
+
+export type IFormData = {
+  toDo: string;
+};
