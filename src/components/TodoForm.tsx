ce7651ca539795df7bab3af6a515b7c9719ce9e6
junny97 <pcohad12@gmail.com>
2023-11-01 19:59:23 +0900

    Feat: input 입력값이 없거나 공백검사 실패시 경고문 출력#3

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -10,6 +10,10 @@ export default function TodoForm() {
   const setToDos = useSetRecoilState(todoState);
   const category = useRecoilValue(categoryState);
   const onSubmit = ({ toDo }: IFormData) => {
+    if (!toDo || !toDo.trim()) {
+      alert('오늘 할 일을 먼저 입력해주세요');
+      return;
+    }
     setToDos((prev) => [{ text: toDo, id: Date.now(), category }, ...prev]);
     setValue('toDo', '');
   };
@@ -26,21 +30,17 @@ export default function TodoForm() {
   );
 }
 const NewToDoInput = styled.input`
-  display: inline-block;
   padding: 10px;
   width: 540px;
   margin-right: 15px;
-  /* color: ${(props) => props.theme.accentColor}; */
-  color: black;
   border: none;
-  background-color: lightgray;
-  border-bottom: 1px solid black;
+  background-color: ${(props) => props.theme.accentColor};
   box-sizing: border-box;
   outline: none;
   border-radius: 8px;
   font-size: 18px;
   &::placeholder {
     font-size: 16px;
-    color: var(--gray-800);
+    color: #979797;
   }
 `;
