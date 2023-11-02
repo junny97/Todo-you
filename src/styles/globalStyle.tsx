ce6373df111aec7744034975b3338bcf56725b84
junny97 <pcohad12@gmail.com>
2023-11-02 21:52:07 +0900

    Style: GlobalStyle 설정 변경& 폰트 설치#2

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -1,74 +1,52 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import '../styles/font.css';
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
+    ${reset};
 
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
+    :root {
+        box-sizing: border-box;
+        font-size: 14px;
 
-button{
-  border: none;
-  cursor: pointer;
+        --gray100-color: #F8F8FC;
+        --gray200-color: #DADADA;
+        --gray300-color: #ACACAC;
+        --gray400-color: #979797;
+        --gray500-color: #4B4B4B;
+        --black-color: #000000;
+        --error-color: #FF3636;
+    };
 
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
+    body {
+        font-family: "Pretendard-Regular", sans-serif;
+    };
 
-*{
-  box-sizing: border-box;
-}
+    a {
+		text-decoration: none;
+		color: inherit;
+	  };
 
-body{
-  font-family: 'Source Sans 3', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
-  line-height: 1.2;
-}
+    button,
+    input {
+        all: unset;
+    };
 
-a{
-  text-decoration: none;
-  color: inherit;
-}
+    img {
+    vertical-align: top;
+    };
+
+  .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  };
+
 `;
+
+export default GlobalStyle;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,6 @@ import { DefaultTheme } from 'styled-components';
 export const lightTheme: DefaultTheme = {
   bgColor: 'white',
   textColor: 'black',
-  borderColor: 'lightgray',
-  accentColor: '#487eb0',
+  borderColor: '#DADADA',
+  accentColor: '#F8F8FC',
 };
